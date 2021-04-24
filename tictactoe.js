1f1775c10633b1240518a4c32c24dbeab6fd53e7
Elina2203 <elina.olsanska@gmail.com>
2021-04-24 12:08:42 +0300

    Added validation

--- tictactoe.js
+++ tictactoe.js
@@ -1,32 +1,42 @@
-let container = document.querySelector(".container");
+let board = document.querySelector(".board");
 let btnReset = document.querySelector(".btn-reset");
+
 let count = 0;
+
+const winningMessageElement = document.getElementById("winningMessage");
+
 for (let i = 0; i <= 8; i++) {
   let element = document.createElement("a");
   element.setAttribute("href", "#");
-  element.setAttribute("data-r", i);
-  element.setAttribute("data-c", i);
-  container.append(element);
-  let elements = document.querySelectorAll(".container a");
+  element.setAttribute("data-id", i);
+  board.append(element);
 
   element.addEventListener("click", function () {
-    count++;
-    if (count > 9) {
-      element.textContent = " ";
-      console.log("game over");
-      count = 0;
-    }
-    if (count % 2 == 0) {
-      element.textContent = "0";
-    } else {
-      element.textContent = "X";
+    if (count > 8) {
+      reset();
     }
 
-    console.log("count:" + count);
+    if (element.textContent != "0" && element.textContent != "X") {
+      count++;
+      if (count % 2 == 0) {
+        element.textContent = "0";
+        element.classList.add(".added_0");
+      } else {
+        element.textContent = "X";
+        element.classList.add(".added_x");
+      }
+
+      console.log("count:" + count);
+    }
   });
-  btnReset.addEventListener("click", function () {
+}
+btnReset.addEventListener("click", reset);
+
+function reset() {
+  let elements = document.querySelectorAll(".board a");
+  for (element of elements) {
     element.textContent = " ";
-    console.log("game over");
-    count = 0;
-  });
+  }
+  console.log("game over");
+  count = 0;
 }
