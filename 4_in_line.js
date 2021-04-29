ca0d873821b67d09bca7782032b91a08154c7bdc
Elina2203 <elina.olsanska@gmail.com>
2021-04-29 09:16:10 +0300

    added comments

--- 4_in_line.js
+++ 4_in_line.js
@@ -1,7 +1,8 @@
+//Создали класс FourInLine
 function FourInLine() {
   this.app = document.getElementById("app");
   this.fields = app.getElementsByTagName("a");
-  let matches = 0;
+  let matches = 0; //количество совпадений
   this.checkWinner = function (id, value) {
     let checks = [
       [
@@ -20,7 +21,7 @@ function FourInLine() {
     for (let check of checks) {
       for (let diffs of check) {
         countMatches.bind(this)(id, value, diffs[0], diffs[1]);
-      }
+      } //с помощью функции bind мы указываем, что this это будет тот же объект
 
       if (matches >= 3) {
         console.log("Winner is " + value);
@@ -44,7 +45,11 @@ function FourInLine() {
 
 let four_in_line = new FourInLine();
 let count = 0;
-
+//для того, чтобы не прописывать обработчик событий для каждой ячейки из нашего поля,
+//мы поставили единый обработчик на все поле. И он будет получать элемент, на котором
+//прошло событие.
+//и дальше прописали условие на какой клик не реагировать
+//если это не ссылка - нажали между ячеек или в ячейке уже есть зачение
 four_in_line.app.addEventListener("click", function (event) {
   let field = event.target;
   if (field.tagName != "A" || field.textContent != "") {
